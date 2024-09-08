255aaca8cfa5fac57aaf8de8e5ae33f593f6ae6d
Huzaifa Nazeer <162676983+dev-huzi-11@users.noreply.github.com>
2024-09-08 10:18:37 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,24 @@
+// const nameInput = document.querySelector("#name") as HTMLElement;
+// const emailInput = document.querySelector("#email") as HTMLElement;
+// const phoneInput = document.querySelector("#phone") as HTMLElement;
+// const educationInput = document.querySelector("#education") as HTMLElement;
+// const skillsInput = document.querySelector("#sills") as HTMLElement;
+// const experienceInput = document.querySelector("#experience") as HTMLElement;
+// const submitButton = document.querySelector("#submit-button") as HTMLButtonElement;
+// const popUpContainer = document.querySelector(".popup-container") as HTMLDivElement;
+
+// submitButton.addEventListener("submit", (event) => {
+//     event.preventDefault()
+//     showPopUp("Form is submited")
+// })
+
+// function showPopUp(message: string){
+//     const submitionPopUp = document.createElement("div") as HTMLDivElement;
+//     submitionPopUp.className = "pop-up";
+//     submitionPopUp.innerHTML = message;
+//     popUpContainer.appendChild(submitionPopUp);
+
+//     setTimeout(() => {
+//         submitionPopUp.remove()
+//     }, 6000)
+// }
